66288381196d8970b48073a487ba3b8db3c5e2a1
payal0505 <sudhakar.payal23@vit.edu>
2025-03-21 12:44:57 +0530

    Add files via upload

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import AdminSidebar from '../components/dashboard/AdminSidebar';
+import Navbar from '../components/dashboard/Navbar';
+import AdminSummary from '../components/dashboard/AdminSummary';
+import SummaryCard from '../components/dashboard/SummaryCard';
+import { Outlet } from 'react-router-dom';
+
+const AdminDashboard = () => {
+   
+    return (
+        <div className='flex'>
+            <AdminSidebar />
+            <div className='flex-1 ml-64 bg-gray-100 h-screen'>
+                <Navbar />
+                <Outlet />
+                
+
+        </div>
+        </div>
+    );
+};
+
+export default AdminDashboard;
