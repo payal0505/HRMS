23d137317be9bf7c87dbe31f4c8f71dbfee0630c
payal0505 <sudhakar.payal23@vit.edu>
2025-03-21 12:19:18 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+],
+theme: {
+    extend: {
+        fontFamily:{
+            "EmblemaOne": ["Emblema One", "sans-serif"],
+        }
+    },
+},
+plugins: [],
+}
