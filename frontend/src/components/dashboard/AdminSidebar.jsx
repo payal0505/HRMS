60192b9d1dd36c3a602c36749a2efda941297dc2
payal0505 <sudhakar.payal23@vit.edu>
2025-03-21 12:33:18 +0530

    Add files via upload

---
+++ frontend/src/components/dashboard/AdminSidebar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaTachometerAlt, FaUsers, FaMoneyBillWave, FaCogs, FaBuilding, FaCalendarAlt} from 'react-icons/fa';
+
+const AdminSidebar = () => {
+    return(
+    <div className="bg-gray-800 text-white h-screen fixed left-0 top-0 bottom-0 space-y-2 w-64">
+       <div className='bg-teal-600 h-12 flex items-center justify-center'>
+
+        <h3 className='text-2xl text-center'>Employee MS</h3>
+       </div>
+       <div>
+        <NavLink to="/admin-dashboard"
+            className={({isActive}) => `${isActive ? "bg-teal-500 " : " "}flex items-center space-x-4 block py-2.5 px-4 rounded`}
+            end>
+            <FaTachometerAlt />
+            <span>Dashboard</span>
+        </NavLink>
+
+        <NavLink to="/admin-dashboard/employees"
+            className={({isActive}) => `${isActive ? "bg-teal-500 " : " "}flex items-center space-x-4 block py-2.5 px-4 rounded`}>
+
+            <FaUsers />
+            <span>Employees</span>
+        </NavLink>
+
+        <NavLink to="/admin-dashboard/departments"
+            className={({isActive}) => `${isActive ? "bg-teal-500 " : " "}flex items-center space-x-4 block py-2.5 px-4 rounded`}>
+
+            <FaBuilding />
+            <span>Departments</span>
+        </NavLink>
+
+        <NavLink to="/admin-dashboard"
+                    className="flex items-center space-x-4 block py-2.5 px-4 rounded">
+
+            <FaCalendarAlt />
+            <span>Leaves</span>
+        </NavLink>  
+
+        <NavLink to="/admin-dashboard/"
+                    className="flex items-center space-x-4 block py-2.5 px-4 rounded">
+
+            <FaMoneyBillWave />
+            <span>Salary</span>
+        </NavLink>
+
+        <NavLink to="/admin-dashboard"
+                    className="flex items-center space-x-4 block py-2.5 px-4 rounded">
+
+            <FaCogs />
+            <span>Settings</span>
+        </NavLink>
+       </div>
+    </div>
+    )
+}
+
+export default AdminSidebar;
