654b817b664a29e7c363ee0a1a25f3def8242cc0
payal0505 <sudhakar.payal23@vit.edu>
2025-04-22 09:51:30 +0530

    Create Attendance.js

---
+++ server/models/Attendance.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const AttendanceSchema = new mongoose.Schema({
+    date: { type: Date, required: true },
+    employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
+    status: { type: String, enum: ['Present', 'Absent', 'Sick', 'Leave'], default: null},
+   
+});
+
+
+const Attendance = mongoose.model('Attendance', AttendanceSchema);
+
+export default Attendance;
