473922c6d2ab8ade58d67e3081d3b8081d7c78be
payal0505 <sudhakar.payal23@vit.edu>
2025-04-22 09:44:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,27 @@ import DepartmentList from "./components/departments/DepartmentList";
 import AddDepartment from "./components/departments/AddDepartment";
 import List from "./components/employee/List";
 import Add from "./components/employee/Add";
+import EditDepartment from "./components/departments/EditDepartment";
+import View from "./components/employee/View";
+import AddSalary from "./components/salary/Add";
+import Edit from "./components/employee/Edit";
+import ViewSalary from "./components/salary/View";
+import Summary from './components/EmployeeDashboard/Summary';
+import LeaveList from './components/leaves/List';
+import AddLeave from './components/leaves/Add';
+import Setting from './components/EmployeeDashboard/Setting';
+import Table from './components/leaves/Table';
+import Detail from './components/leaves/Detail';
+import AdminChangePassword from "./components/AdminChangePassword/AdminChangePassword";
+import Attendance from "./components/attendance/Attendance";
+import AttendanceReport from "./components/attendance/AttendanceReport";
+import AssignProject from './components/project/AssignProject';
+import Projects from './components/project/EmployeeProjects';
+import ProjectList from "./components/project/ProjectList";
+
+
+
+
 
 function App() {
   return (
@@ -17,9 +38,11 @@ function App() {
  <Routes>
   <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
   <Route path="/login" element={<Login />}></Route>
+
+
   <Route path="/admin-dashboard" element={
     <PrivateRoutes>
-      <RoleBaseRoutes requiredRole={["admin"]}>
+      <RoleBaseRoutes requiredRole={["admin", "employee"]}>
 
     <AdminDashboard />
      </RoleBaseRoutes>
@@ -30,14 +53,56 @@ function App() {
 
 <Route path="/admin-dashboard/departments" element={<DepartmentList />}></Route>
 <Route path="/admin-dashboard/add-department" element={<AddDepartment />}></Route>
+<Route path="/admin-dashboard/department/:id" element={<EditDepartment />}></Route>
+
 
 <Route path="/admin-dashboard/employees" element={<List />}></Route>
 <Route path="/admin-dashboard/add-employee" element={<Add />}></Route>
+<Route path="/admin-dashboard/employees/:id" element={<View />}></Route>
+<Route path="/admin-dashboard/employees/edit/:id" element={<Edit />}></Route>
+<Route path="/admin-dashboard/employees/salary/:id" element={<ViewSalary />}></Route>
 
 
+
+<Route path="/admin-dashboard/salary/add" element={<AddSalary />}>
 </Route>
-  <Route path="/employee-dashboard" element={<EmployeeDashboard />}></Route>
+
+<Route path="/admin-dashboard/leaves" element={<Table />}> </Route>
+<Route path="/admin-dashboard/leaves/:id" element={<Detail />}> </Route>
+<Route path="/admin-dashboard/employees/leaves/:id" element={<LeaveList />}> </Route>
+<Route path="/admin-dashboard/settings" element={<AdminChangePassword />} />
+<Route path="/admin-dashboard/attendance" element={<Attendance />}></Route>
+<Route path="/admin-dashboard/attendance-report" element={<AttendanceReport />}></Route>
+<Route path="/admin-dashboard/projects" element={<AssignProject />} />
+<Route path="/admin-dashboard/project-list" element={<ProjectList />} />
+
+
+
+  </Route>
+  <Route path="/employee-dashboard"
+  element={
+  <PrivateRoutes>
+    <RoleBaseRoutes requiredRole={["admin", "employee"]}>
+    <EmployeeDashboard />
+    </RoleBaseRoutes>
+  </PrivateRoutes>
+  }
+  >
+
+<Route index element={<Summary />}></Route>
+
+<Route path="/employee-dashboard/profile/:id" element={<View />}></Route>
+<Route path="/employee-dashboard/leaves/:id" element={<LeaveList />}></Route>
+<Route path="/employee-dashboard/add-leaves" element={<AddLeave />}></Route>
+<Route path="/employee-dashboard/salary/:id" element={<ViewSalary />} />
+<Route path="/employee-dashboard/projects/:id" element={<Projects />} />
+<Route path="/employee-dashboard/setting" element={<Setting />} />
+
+
+
+ </Route>
  </Routes>
+  
  </BrowserRouter>
   );
 }
