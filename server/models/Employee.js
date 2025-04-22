5d4a047f4996302aecf3260cdbfb89c05926978d
payal0505 <sudhakar.payal23@vit.edu>
2025-04-22 09:51:46 +0530

    Create Employee.js

---
+++ server/models/Employee.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const employeeSchema = new Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    employeeId: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    dob: { type: Date },
+    gender: { type: String },
+    maritalStatus: { type: String },
+    designation: { type: String },
+    department: { type: mongoose.Schema.Types.ObjectId, ref: "Department", required: true },
+    salary: { type: Number, required: true },
+    role: { type: String, enum: ["admin","employee"], required: true }, 
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+const Employee = mongoose.model("Employee", employeeSchema);
+export default Employee;
